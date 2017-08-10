01a4c06edc9c7e8bda1c863f104de51e081767c0
Chen Xiaofeng <515318881@qq.com>
2017-08-10 15:38:25 +0800

    OK
    记得另一路串口要初始化且用中断方式接收

--- Framework/drivers/drivers_uart.c
+++ Framework/drivers/drivers_uart.c
@@ -16,10 +16,8 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *UartHandle)
 		//rcUartRxCpltCallback();
 		fw_Warning();
 	}else if(UartHandle == &CTRL_UART){
-		//testuart = 1;
 		ctrlUartRxCpltCallback();
 	}else if(UartHandle == &JUDGE_UART){
-		//testuart = 1;
 		//HAL_UART_Receive_IT(&JUDGE_UART, &tmp_judge, 1);
 		judgeUartRxCpltCallback();
 	}
